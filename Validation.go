acf19a367a26337c596e94597e41e9cf42223e95
opsconquer <527628614@qq.com>
2016-02-03 16:35:04 +0800

    Update Validation.go

--- Validation.go
+++ Validation.go
@@ -95,7 +95,9 @@ return false
 }
 
 
-
+fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
 
 
 
