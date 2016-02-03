c28058fb22ad7ee12c8ba69296caca28f6f09e90
opsconquer <527628614@qq.com>
2016-02-03 15:47:01 +0800

    Update Validation.go

--- Validation.go
+++ Validation.go
@@ -41,6 +41,73 @@ if m, _ := regexp.MatchString(`^(1[3|4|5|8][0-9]\d{4,8})$`, r.Form.Get("mobile")
 return false
 }
 
+下来菜单
+<select name="fruit">
+<option value="apple">apple</option>
+<option value="pear">pear</option>
+<option value="banane">banane</option>
+</select>
+验证
+slice:=[]string{"apple","pear","banane"}
+for _, v := range slice {
+if v == r.Form.Get("fruit") {
+return true
+}
+}
+return false
+
+单选按钮
+<input type="radio" name="gender" value="1">男
+<input type="radio" name="gender" value="2">女
+验证
+slice:=[]int{1,2}
+for _, v := range slice {
+if v == r.Form.Get("gender") {
+return true
+}
+}
+return false
+
+复选框
+<input type="checkbox" name="interest" value="football">足球
+<input type="checkbox" name="interest" value="basketball">篮球
+<input type="checkbox" name="interest" value="tennis">网球
+验证:
+slice:=[]string{"football","basketball","tennis"}
+a:=Slice_diff(r.Form["interest"],slice)
+if a == nil{
+return true
+}
+return false
+
+日期和时间
+t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+fmt.Printf("Go launched at %s\n", t.Local())
+
+身份证号码
+//验证15位身份证，15位的是全部数字
+if m, _ := regexp.MatchString(`^(\d{15})$`, r.Form.Get("usercard")); !m {
+return false
+}
+//验证18位身份证，18位前17位为数字，最后一位是校验位，可能为数字或字符X。
+if m, _ := regexp.MatchString(`^(\d{17})([0-9]|X)$`, r.Form.Get("usercard")); !m {
+return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
