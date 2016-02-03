304b1b15525804040b2897c4a0a6b7fbeaa8691e
opsconquer <527628614@qq.com>
2016-02-03 17:31:06 +0800

    Update Validation.go

--- Validation.go
+++ Validation.go
@@ -123,6 +123,41 @@ Hello, &lt;script&gt;alert(&#39;you have been pwned&#39;)&lt;/script&gt;!
 
 
 防止多次递交表单
+<input type="checkbox" name="interest" value="football">足球
+<input type="checkbox" name="interest" value="basketball">篮球
+<input type="checkbox" name="interest" value="tennis">网球
+用户名:<input type="text" name="username">
+密码:<input type="password" name="password">
+<input type="hidden" name="token" value="{{.}}">
+<input type="submit" value="登陆">
+增加了一个隐藏字段token ，这个值我们通过MD5(时间戳)来获取惟一值
+func login(w http.ResponseWriter, r *http.Request) {
+fmt.Println("method:", r.Method) //获取请求的方法
+if r.Method == "GET" {
+crutime := time.Now().Unix()
+h := md5.New()
+io.WriteString(h, strconv.FormatInt(crutime, 10))
+token := fmt.Sprintf("%x", h.Sum(nil))
+t, _ := template.ParseFiles("login.gtpl")
+t.Execute(w, token)
+} else {
+//请求的是登陆数据，那么执行登陆的逻辑判断
+r.ParseForm()
+token := r.Form.Get("token")
+if token != "" {
+//验证token的合法性
+} else {
+//不存在token报错
+}
+fmt.Println("username length:", len(r.Form["username"][0]))
+fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+}
+}
+
+
+
 
 
 
